d67b2acfcab209d8416f25b82451600a61cc59e0
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2021-12-03 09:01:32 +0100

    Positioned items

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,6 +8,19 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+void setItemsPosition()
+{
+    Ball->Top = (Form1->ClientHeight) / 2 - (Ball->Height) / 2;
+    Ball->Left = (Form1->ClientWidth) / 2 - (Ball->Width) / 2;
+
+    LeftPaddle->Top = (Form1->ClientHeight) / 2 - (LeftPaddle->Height) / 2;
+    LeftPaddle->Left = 20;
+
+    RightPaddle->Top = (Form1->ClientHeight) / 2 - (RightPaddle->Height) / 2;
+    RightPaddle->Left = Form1->ClientWidth - 40;
+}
+
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
     : TForm(Owner)
@@ -19,7 +32,6 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 
 void __fastcall TForm1::FormCreate(TObject *Sender)
 {
-    Ball->Top = (Form1->ClientHeight) / 2;
-    Ball->Left = (Form1->ClientWidth) / 2;
+    setItemsPosition();
 }
 //---------------------------------------------------------------------------
